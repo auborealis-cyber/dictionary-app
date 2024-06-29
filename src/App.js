7f4662df71dab81c4a4cafeaef91fbb7519e0858
auborealis-cyber <149447678+auborealis-cyber@users.noreply.github.com>
2024-06-29 13:24:33 -0700

    Developing search engine
    Created form and making sure keyword works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from "./Au-logo.png";
 import './App.css';
+import Dictionary from "./Dictionary"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo"/>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary/>
+        </main>
+        <footer className="App-footer text-center">
+          Coded with ❤️ by Aurora Thomas, open-sourced on GitHub and hosted by{" "}
+          <a href="https://audictionary-app.netlify.app/">Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
