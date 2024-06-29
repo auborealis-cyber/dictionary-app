320c19401d14b18f85ed8fb9c5daeec088c356c9
auborealis-cyber <149447678+auborealis-cyber@users.noreply.github.com>
2024-06-29 14:04:28 -0700

    Integrated API
    Added dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
 
-    function search(event) {
-        event.preventDefault();
-        alert(`Searching for ${keyword}`)
+    function handleResponse(response) {
+      console.log(response.data.meanings[0]);
+    
     }
+
+    function search(event) {
+      event.preventDefault();
+
+      //API doc https://www.shecodes.io/learn/apis/dictionary
+      let apiKey = "4f0tbb34c0ea3deo2dcac0706f4ad715";
+      let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+      axios.get(apiUrl).then(handleResponse);
+    }
+
+
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
